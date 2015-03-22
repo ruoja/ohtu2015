6f2daffbb394edc05301cb108ee9f0fa5d6de67f
ruoja <ruoja74@gmail.com>
2015-03-22 22:52:38 +0200

    annotaatiot

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/OmaViitegeneraattori.java
@@ -0,0 +1,18 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OmaViitegeneraattori implements Viitegeneraattori {
+  
+    private int seuraava;
+    
+    public OmaViitegeneraattori(){
+        seuraava = 1;    
+    }
+    
+    @Override
+    public int uusi(){
+        return seuraava++;
+    }
+}
