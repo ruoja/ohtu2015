690da7590441509a6c9cea18145b19d4da5eaa80
ruoja <ruoja74@gmail.com>
2015-04-19 22:44:39 +0300

    viikko 5 valmis

--- viikko5/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ viikko5/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -1,7 +1,48 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.client.HttpClients;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String studentNr = "014008742";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "http://ohtustats-2015.herokuapp.com/students/"+studentNr+"/submissions";
+
+        HttpClient client = HttpClients.createDefault();
+        HttpGet method = new HttpGet(url);
+        HttpResponse response = client.execute(method);
+
+        HttpEntity entity =  response.getEntity();
+
+        String bodyText = IOUtils.toString(entity.getContent());
+
+        //System.out.println("json-muotoinen data:");
+        //System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+
+        System.out.println("opiskelijanumero: " + studentNr + "\n");
+        int countAssignments = 0;
+        int countHours = 0;
+        for (Submission submission : subs) {
+            System.out.println(submission);
+            countAssignments = countAssignments + submission.numberOfDone();
+            countHours = countHours + submission.hours();
+        }
+        
+        System.out.println("\nyhteensä: " + countAssignments + " tehtävää " + countHours + " tuntia");
+
     }
 }
--- viikko5/TyhjaProjekti/src/main/java/ohtu/Submission.java
+++ viikko5/TyhjaProjekti/src/main/java/ohtu/Submission.java
@@ -1,7 +1,34 @@
 package ohtu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Submission {
+
     private String student_number;
+    private int week;
+    private int hours;
+    private boolean a1;
+    private boolean a2;
+    private boolean a3;
+    private boolean a4;
+    private boolean a5;
+    private boolean a6;
+    private boolean a7;
+    private boolean a8;
+    private boolean a9;
+    private boolean a10;
+    private boolean a11;
+    private boolean a12;
+    private boolean a13;
+    private boolean a14;
+    private boolean a15;
+    private boolean a16;
+    private boolean a17;
+    private boolean a18;
+    private boolean a19;
+    private boolean a20;
+    private boolean a21;
 
     public String getStudent_number() {
         return student_number;
@@ -11,9 +38,37 @@ public class Submission {
         this.student_number = student_number;
     }
 
-    @Override
-    public String toString() {
-        return student_number;
+    private List done() {
+        boolean[] all = {a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21};
+        List done = new ArrayList();
+        for (int i = 0; i < all.length; i++) {
+            if(all[i]) {
+                done.add(String.valueOf(i + 1));
+            }
+        }
+        return done;
     }
     
+    private String doneToString(List<String> done) {
+        String output = "";
+        for(String assignment : done) {
+            output = output.concat(assignment + " ");
+        }
+        return output.trim();
+    }
+    
+    public int numberOfDone() {
+        return done().size();
+    }
+    
+    public int hours() {
+        return hours;
+    }
+
+    @Override
+    public String toString() {
+        return "viikko " + week + ": " + "tehtyjä tehtäviä yhteensä: " + numberOfDone() + ", aikaa kului " + hours + " tuntia, \n"
+                + "tehdyt tehtävät: " + doneToString(done());
+    }
+
 }
